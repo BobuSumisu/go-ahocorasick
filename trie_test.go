9ec752a1e37fa62cd0a0d4e872285a5e645ebcbf
Øyvind Ingvaldsen <oyvind.ingvaldsen@gmail.com>
2016-09-01 03:43:50 +0200

    gotta fix tests, sleep now

--- trie_test.go
+++ trie_test.go
@@ -5,34 +5,28 @@ import (
 	"testing"
 )
 
-func TestFuckAll(t *testing.T) {
-	trie := NewTrieBuilder(256).AddStringPatterns([]string{
-		"foo",
-		"bar",
-		"baz",
-		"football",
-		"bazar",
-		"øyvind",
-		"foot",
-		"ball",
-	}).Build()
-	trie.MatchString("hei du")
-	// tr.Build()
-	// tr.Match("footballz and bazars are bar foobar")
-}
-
 func TestWiki(t *testing.T) {
 	trie := NewTrieBuilder(256).AddStringPatterns([]string{
 		"a", "ab", "bab", "bc", "bca", "c", "caa",
 	}).Build()
-	trie.MatchString("alle barna gikk i skogen caa bca c")
+	trie.MatchString("abba")
 }
 
 func TestPrefix(t *testing.T) {
 	trie := NewTrieBuilder(256).
 		AddStringPatterns([]string{"Aho-Corasick", "Aho-Cora", "Aho", "A"}).
 		Build()
+	matches := trie.MatchString("Aho-Corasick")
 
+	if len(matches) != 4 {
+		t.Errorf("expected %d matches, got %d", 4, len(matches))
+	}
+}
+
+func TestSuffix(t *testing.T) {
+	trie := NewTrieBuilder(256).
+		AddStringPatterns([]string{"Aho-Corasick", "-Corasick", "rasick", "k"}).
+		Build()
 	matches := trie.MatchString("Aho-Corasick")
 
 	if len(matches) != 4 {
