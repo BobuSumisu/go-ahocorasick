a4488d592713719c97ea6c35fd87ffa7c088058b
Øyvind Ingvaldsen <oyvind.ingvaldsen@gmail.com>
2019-07-09 12:50:21 +0200

    added license and fixed a test

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Aho-Corasick string search algorithm implemented in Go.
 Uses a [double array trie](https://linux.thai.net/~thep/datrie/datrie.html) for improved
 access speeds and reduced memory consumption.
 
+Licensed under MIT License.
+
 ## Documentation
 
 Can be found [here](https://godoc.org/github.com/BobuSumisu/go-ahocorasick).
--- grapher_test.go
+++ grapher_test.go
@@ -13,10 +13,14 @@ func ExampleTrieGrapher_Graph() {
 	grapher := NewTrieGrapher(trie).DrawFailLinks(true)
 	grapher.Graph("trie.dot")
 
-	if err := exec.Command("dot", "-Tpng", "-o", "trie.png", "trie.dot").Run(); err != nil {
-		fmt.Println(err)
+	if _, err := exec.LookPath("dot"); err == nil {
+		if err := exec.Command("dot", "-Tpng", "-o", "trie.png", "trie.dot").Run(); err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println("OK")
+		}
+		// Output: OK
 	} else {
-		fmt.Println("OK")
+		// Output:
 	}
-	// Output: OK
 }
